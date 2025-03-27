b80c55d35ca74f28c0ed5bef97fdf23c55f54ab7
SDaKko <danilkosenko172004@mail.ru>
2025-03-28 02:00:50 +0300

    lab5 4-5: Рекурсия, сумма цифр числа

---
+++ FuncAndRec/SumNums.fs
@@ -0,0 +1,19 @@
+open System
+let rec sumNums n =
+    if n = 0 then 0
+    else (n%10) + (sumNums (n / 10))
+
+let rec sumNumsTail n acc =
+    if n = 0 then acc
+    else sumNumsTail (n/10) (acc + (n%10))
+
+let sumNumsTail2 n =
+    sumNumsTail n 0
+
+[<EntryPoint>]
+let main args =
+    let sum1 = sumNums 1230
+    let sum2 = sumNumsTail2 1230
+    Console.WriteLine($"Рекурсия вниз: {sum1}")
+    Console.WriteLine($"Хвостовая рекурсия: {sum2}")
+    0
