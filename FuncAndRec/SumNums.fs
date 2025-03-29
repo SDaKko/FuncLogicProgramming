de8641120f2c2e800b7e58efd92b56fe24a38fc5
SDaKko <danilkosenko172004@mail.ru>
2025-03-29 09:22:04 +0300

    lab 5 4-5: Рекурсия, изменен вывод

--- FuncAndRec/SumNums.fs
+++ FuncAndRec/SumNums.fs
@@ -1,4 +1,4 @@
-open System
+﻿open System
 let rec sumNums n =
     if n = 0 then 0
     else (n%10) + (sumNums (n / 10))
@@ -14,6 +14,6 @@ let sumNumsTail2 n =
 let main args =
     let sum1 = sumNums 1230
     let sum2 = sumNumsTail2 1230
-    Console.WriteLine($"Рекурсия вниз: {sum1}")
-    Console.WriteLine($"Хвостовая рекурсия: {sum2}")
+    Console.WriteLine($"Рекурсия вверх: {sum1}")
+    Console.WriteLine($"Хвостовая рекурсия (вниз): {sum2}")
     0
