28a679cf67d80bc07b7c80701e242af22337d017
SDaKko <danilkosenko172004@mail.ru>
2025-04-02 01:28:21 +0300

    lab5 11-12: Любимый язык

---
+++ FuncAndRec/Language.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+let askUser answer =
+    match answer with
+    | "F#" | "Prolog" -> "Вы подлиза!"
+    | _ -> "Хороший выбор!"
+
+
+[<EntryPoint>]
+let main args =
+    let userInput = Console.ReadLine()
+    let answer = askUser userInput
+    Console.WriteLine(answer)
+
+    (Console.WriteLine("Какой твой любимый язык программирования?"))
+    |> fun _ -> System.Console.ReadLine()
+    |> askUser
+    |> Console.WriteLine
+    0
