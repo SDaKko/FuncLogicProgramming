cc95d98390f1f5ea13c6bc1f6e9f49b129df939c
SDaKko <danilkosenko172004@mail.ru>
2025-04-23 21:56:23 +0300

    lab8 1: Добавлены правила

---
+++ Prolog/predicates.pl
@@ -0,0 +1,13 @@
+% Задание 1
+man :- man(X), print(X), nl, fail.
+woman :- woman(X), print(X), nl, fail.
+children(X) :- parent(X, Y), print(Y), nl, fail.
+mother(X, Y) :- parent(X, Y), woman(X).
+father(X, Y) :- parent(X, Y), man(X).
+mother(X) :- mother(Y, X), print(Y).
+father(X) :- father(Y, X), print(Y).
+brother(X, Y) :- X\=Y, man(X), father(F, X), father(F, Y), mother(M, X), mother(M, Y).
+brothers(X) :- father(F, X), mother(M, X), father(F, Brother), 
+mother(M, Brother), man(Brother), print(Brother), nl, fail.
+b_s(X, Y) :- father(F, X), father(F, Y), mother(M, X), mother(M, Y), X \= Y.
+b_s(X) :- b_s(X, Y), print(Y), nl, fail.
