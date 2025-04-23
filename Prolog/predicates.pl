00ec2abea537034d59cd8c77571d04a4eb8942fd
SDaKko <danilkosenko172004@mail.ru>
2025-04-23 23:24:25 +0300

    lab8 2: Предикаты по вариантам

--- Prolog/predicates.pl
+++ Prolog/predicates.pl
@@ -11,3 +11,9 @@ brothers(X) :- father(F, X), mother(M, X), father(F, Brother),
 mother(M, Brother), man(Brother), print(Brother), nl, fail.
 b_s(X, Y) :- father(F, X), father(F, Y), mother(M, X), mother(M, Y), X \= Y.
 b_s(X) :- b_s(X, Y), print(Y), nl, fail.
+
+% Задание 2. Вариант 3
+daughter(X, Y) :- woman(X), parent(Y, X), !.
+daughter(X) :- daughter(Y, X), print(Y).
+wife(X, Y) :- woman(X), man(Y), parent(X, C), parent(Y, C), !.
+wife(X) :- wife(Y, X), print(Y).
