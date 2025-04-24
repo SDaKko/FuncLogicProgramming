7fa595cf9cebc0f1d95865f3840d49bc1d3655e6
SDaKko <danilkosenko172004@mail.ru>
2025-04-24 20:05:04 +0300

    lab8 3: Новые предикаты

--- Prolog/predicates.pl
+++ Prolog/predicates.pl
@@ -17,3 +17,24 @@ daughter(X, Y) :- woman(X), parent(Y, X), !.
 daughter(X) :- daughter(Y, X), print(Y).
 wife(X, Y) :- woman(X), man(Y), parent(X, C), parent(Y, C), !.
 wife(X) :- wife(Y, X), print(Y).
+
+% Задание 3. Вариант 3
+% С предикатами
+grand_so(X, Y) :- father(P, X), father(Y, P), man(X).
+grand_sons(X) :- grand_so(Y, X), print(Y), nl, fail.
+
+grand_ma_and_son(X, Y) :- (woman(X), grand_so(Y, X)) ; (woman(Y), grand_so(X, Y)).
+
+uncle(X, Y) :- b_s(X, F), parent(F, Y), man(X).
+uncles(X) :- uncle(Y, X), print(Y), nl, fail.
+
+% С фактами
+
+grand_so(X, Y) :- parent(P, X), parent(Y, P), man(X).
+grand_sons(X) :- parent(X, P),  parent(P, Y), man(Y), print(Y), nl, fail.
+
+grand_ma_and_son(X, Y) :- (woman(X), parent(P, Y), parent(X, P), man(X)) ; (woman(Y), parent(P, X), parent(Y, P), man(X)).
+
+% Здесь 2 раза может быть true, так как сначала находит общего отца, затем общую мать, можно сделать отсечение
+uncle(X, Y) :- parent(F, X), parent(F, P), parent(P, Y), man(X), !.
+uncles(X) :- parent(F, X), parent(P, F), parent(P, D), man(P), D \= F, man(D), print(D), nl, fail.
